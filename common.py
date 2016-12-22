fb276b987851f749a79d084741c236c4caeb13a7
Hellux <hellux07@gmail.com>
2016-12-22 20:47:47 +0100

    added day3, day4

--- common.py
+++ common.py
@@ -5,3 +5,19 @@ def read_file(path):
 
 def parse_input(day):
     return read_file('input/{:02d}.txt'.format(day))
+
+
+def quicksort(seq):
+    if len(seq) <= 1: return seq
+
+    pivot = seq[0]
+    lesser = []
+    equal = []
+    greater = []
+
+    for scalar in seq:
+        if scalar < pivot: lesser.append(scalar)
+        elif scalar > pivot: greater.append(scalar)
+        else: equal.append(scalar)
+
+    return quicksort(lesser) + equal + quicksort(greater)
