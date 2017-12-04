090ff938a5d4ffb664c7d58ca47d458e3b487b3c
Hellux <hellux07@gmail.com>
2017-12-04 12:47:33 +0100

    added uncommited stuff

---
+++ day12_assembunny.py
@@ -0,0 +1,115 @@
+"""Day 12: Leonardo's Monorail
+
+You finally reach the top floor of this building: a garden with a slanted glass
+ceiling. Looks like there are no more stars to be had.
+
+While sitting on a nearby bench amidst some tiger lilies, you manage to decrypt
+some of the files you extracted from the servers downstairs.
+
+According to these documents, Easter Bunny HQ isn't just this building - it's a
+collection of buildings in the nearby area. They're all connected by a local
+monorail, and there's another building not far from here! Unfortunately, being
+night, the monorail is currently not operating.
+
+You remotely connect to the monorail control systems and discover that the
+boot sequence expects a password. The password-checking logic (your puzzle
+input) is easy to extract, but the code it uses is strange: it's assembunny
+code designed for the new computer you just assembled. You'll have to execute
+the code and get the password.
+
+The assembunny code you've extracted operates on four registers (a, b, c, and d)
+that start at 0 and can hold any integer. However, it seems to make use of
+only a few instructions:
+
+-cpy x y copies x (either an integer or the value of a register) into register
+ y.
+-inc x increases the value of register x by one.
+-dec x decreases the value of register x by one.
+-jnz x y jumps to an instruction y away (positive means forward; negative
+ means backward), but only if x is not zero.
+-The jnz instruction moves relative to itself: an offset of -1 would continue
+ at the previous instruction, while an offset of 2 would skip over the next
+ instruction.
+
+For example:
+
+cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a
+
+The above code would set register a to 41, increase its value by 2, decrease
+its value by 1, and then skip the last dec a (because a is not zero, so the
+jnz a 2 skips it), leaving register a at 42. When you move past the last
+instruction, the program halts.
+
+After executing the assembunny code in your puzzle input, what value is left
+in register a?
+
+--- Part Two ---
+
+As you head down the fire escape to the monorail, you notice it didn't start;
+register c needs to be initialized to the position of the ignition key.
+
+If you instead initialize register c to be 1, what value is now left in
+register a?
+"""
+
+from common import parse_input
+
+
+def run_program(program, variables):
+    return eval_program(parse_program(program), variables)
+
+
+def parse_program(program):
+    return [statement.split() for statement in program.split('\n')]
+
+
+def eval_program(program, variables):
+    pointer = 0
+    
+    while pointer != len(program):
+        statement = program[pointer]
+        op = statement[0]
+        if op == 'cpy':
+            x, y = statement[1:]
+            if x.isalpha():
+                x = variables[x]
+            variables[y] = int(x)
+        else:
+            reg = statement[1]
+            if op == 'inc':
+                variables[reg] += 1
+            elif op == 'dec':
+                variables[reg] -= 1
+            elif op == 'jnz':
+                jump = int(statement[2])
+                x = variables[reg] if reg.isalpha() else int(reg)
+                if x:
+                    pointer += jump
+                    continue
+        pointer += 1
+
+    return variables
+
+
+def part_one(program):
+    variables = {var : 0 for var in 'abcd'}
+    variables = run_program(program, variables)
+    print('Part one -- a:', variables['a'])
+
+ 
+def part_two(program):
+    variables = {var : 0 for var in 'abcd'}
+    variables['c'] = 1
+    variables = run_program(program, variables)
+    print('Part two -- a:', variables['a'])
+
+
+if __name__ == '__main__':
+    program = parse_input(12)
+    #part_one(program)
+    part_two(program)
---
+++ day13_pathfind.py
@@ -0,0 +1,128 @@
+"""--- Day 13: A Maze of Twisty Little Cubicles ---
+
+You arrive at the first floor of this new building to discover a much less
+welcoming environment than the shiny atrium of the last one. Instead, you are
+in a maze of twisty little cubicles, all alike.
+
+Every location in this area is addressed by a pair of non-negative integers (x
+y). Each such coordinate is either a wall or an open space. You can't move
+diagonally. The cube maze starts at 0,0 and seems to extend infinitely toward
+positive x and y; negative values are invalid, as they represent a location
+outside the building. You are in a small waiting area at 1,1.
+
+While it seems chaotic, a nearby morale-boosting poster explains, the layout
+is actually quite logical. You can determine whether a given x,y coordinate
+will be a wall or an open space using a simple system:
+
+-Find x*x + 3*x + 2*x*y + y + y*y.
+-Add the office designer's favorite number (your puzzle input).
+-Find the binary representation of that sum; count the number of bits that are
+ 1.
+-If the number of bits that are 1 is even, it's an open space.
+-If the number of bits that are 1 is odd, it's a wall.
+-For example, if the office designer's favorite number were 10, drawing walls
+ as # and open spaces as ., the corner of the building containing 0,0 would
+ look like this:
+
+  0123456789
+0 .#.####.##
+1 ..#..#...#
+2 #....##...
+3 ###.#.###.
+4 .##..#..#.
+5 ..##....#.
+6 #...##.###
+
+Now, suppose you wanted to reach 7,4. The shortest route you could take is
+marked as O:
+
+  0123456789
+0 .#.####.##
+1 .O#..#...#
+2 #OOO.##...
+3 ###O#.###.
+4 .##OO#OO#.
+5 ..##OOO.#.
+6 #...##.###
+
+Thus, reaching 7,4 would take a minimum of 11 steps (starting from your
+current location, 1,1).
+
+What is the fewest number of steps required for you to reach 31,39?
+"""
+
+from common import parse_input
+from time import sleep
+
+WALL = '█'
+FLOOR = ' '
+
+def is_open(x, y, seed):
+    number = x*x + 3*x + 2*x*y + y + y*y + seed
+    return bin(number).count('1') % 2 == 0
+
+
+def draw_room(start_x, start_y, end_x, end_y, seed, markers=None):
+    room = [[FLOOR if is_open(x, y, seed) else WALL
+             for x in range(start_x, end_x)]
+            for y in range(start_y, end_y)]
+
+    if markers:
+        for marker, positions in markers.items():
+            for x, y in positions:
+                if start_x < x < end_x and \
+                   start_y < y < end_y:
+                    room[y-start_y][x-start_x] = marker
+
+    for row in room:
+        print(''.join(row))
+
+
+def render(x, y, previous, options, goal, seed):
+    w, h = 160, 55
+    #sleep(0.3)
+    r = lambda x: round(x/30)*30
+    draw_room(r(x-w//2), r(y-h//2), r(x+w//2), r(y+h//2), seed, {
+        '▓': ((x, y),),
+        '▒': previous,
+        '░': options,
+        'G': (goal,)}
+    )
+
+
+def distance(a, b):
+    return ((a[0]-b[0])**2+(a[1]-b[1])**2)**(1/2)
+
+
+def find_shortest_path(goal, seed, start=(1, 1), previous=tuple()):
+
+    sx, sy = start
+    options = [(sx+x, sy+y) \
+               for x, y in ((0, 1), (0, -1), (1, 0), (-1, 0)) \
+               if is_open(sx+x, sy+y, seed) and \
+                  (sx+x, sy+y) not in previous]
+
+    #render(*start, previous, options, goal, seed)
+
+    if not options:
+        return None
+    elif goal in options or start == goal:
+        return 1
+    else:
+        for option in sorted(options, key=lambda x: distance(x, goal)):
+            path = find_shortest_path(goal, seed, option, previous+(start,))
+            if path:
+                return 1+path
+        else:
+            return None
+
+
+def part_one(seed):
+    print('Part one -- Shortest path length:',
+          find_shortest_path((31, 39), seed))
+
+
+if __name__ == '__main__':
+    seed = int(parse_input(13))
+    seed = 1364
+    part_one(seed)
