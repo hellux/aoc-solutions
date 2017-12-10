2648e5f53bdb9988c7bfcca8444f08a6569cc3af
Hellux <hellux07@gmail.com>
2017-12-10 17:19:39 +0100

    skipped run on failed compile

--- 2017/cr_hs.sh
+++ 2017/cr_hs.sh
@@ -1,5 +1,6 @@
-ghc -dynamic -o a.out $1_*.hs
-echo OUTPUT:
-echo -----------------------
-cat input/$1.txt | ./a.out
-rm *.hi *.o
+ghc -dynamic -o a.out $1_*.hs && {
+    echo OUTPUT:
+    echo -----------------------
+    cat input/$1.txt | ./a.out
+    rm *.hi *.o
+}
