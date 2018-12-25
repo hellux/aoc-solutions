7b1d2f7d814c7ff77f56930a65a73c84b26041c1
Noah Hellman <noah.hellman@protonmail.com>
2018-12-25 10:41:02 +0100

    add option for custom input files

--- 2018/run.sh
+++ 2018/run.sh
@@ -1,9 +1,11 @@
 # usage: ./run.sh day or ./run.sh -i "INPUT" day
 
 input=""
-while getopts i: flag; do
+input_file=""
+while getopts i:I: flag; do
     case "$flag" in
         i) input=$OPTARG;;
+        I) input_file=$OPTARG;;
     esac
 done
 shift $((OPTIND-1))
@@ -11,6 +13,9 @@ shift $((OPTIND-1))
 day=$1
 day_dir="$(echo day$(printf "%02d" "$day")*)"
 
-[ -z "$input" ] && input="$(cat "$day_dir/input")"
+if [ -z "$input" ]; then
+    [ -z "$input_file" ] && input_file="$day_dir/input"
+    input="$(cat "$input_file")"
+fi
 
 printf "$input" | go run "$day_dir/solution.go"
