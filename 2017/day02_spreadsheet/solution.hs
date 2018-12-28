9742d73126054d80c9cf2e0d2ec93e95dfa3abe3
Noah Hellman <noah.hellman@protonmail.com>
2018-12-28 20:05:32 +0100

    add 2017 day 2 part2

--- 2017/day02_spreadsheet/solution.hs
+++ 2017/day02_spreadsheet/solution.hs
@@ -1,7 +1,19 @@
-part1 = sum
-    . map ((\row -> maximum row - minimum row)
-    . map (read::String->Int) . words)
-    . lines
+import Data.List (sort)
+
+part1 :: [[Int]] -> Int
+part1 = sum . map (\row -> maximum row - minimum row)
+
+part2 :: [[Int]] -> Int
+part2 = sum . map (quotient . sort)
+quotient :: [Int] -> Int
+quotient (x:xs) = sum (map f xs) + quotient xs
+    where f y = if mod y x == 0 then y `div` x else 0
+quotient x = 0
 
 main :: IO ()
-main = interact $ show . part1
+main = do
+    input <- getContents
+    let sheet = (map (map (read::String->Int) . words) . lines) input
+
+    print $ part1 sheet
+    print $ part2 sheet
