b01158ffb94c9739fb0d998cb662fcbe76e4c765
Noah Hellman <noah.hellman@protonmail.com>
2018-12-29 18:10:48 +0100

    replace recursion with list comprehension

--- 2017/day02_spreadsheet/solution.hs
+++ 2017/day02_spreadsheet/solution.hs
@@ -4,11 +4,8 @@ part1 :: [[Int]] -> Int
 part1 = sum . map (\row -> maximum row - minimum row)
 
 part2 :: [[Int]] -> Int
-part2 = sum . map (quotient . sort)
-quotient :: [Int] -> Int
-quotient (x:xs) = sum (map f xs) + quotient xs
-    where f y = if mod y x == 0 then y `div` x else 0
-quotient x = 0
+part2 = sum . map q
+    where q row = sum [y `div` x | y <- row, x <- row, y > x, y `mod` x == 0]
 
 main :: IO ()
 main = do
