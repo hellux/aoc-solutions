a1e5c908556874167dda2489050af1ece2ac4e61
Noah Hellman <noah.hellman@protonmail.com>
2018-12-29 18:10:22 +0100

    replace if else with guards

--- 2017/day01_captcha/solution.hs
+++ 2017/day01_captcha/solution.hs
@@ -1,23 +1,21 @@
 import Data.Char (digitToInt)
 
 checksum :: [Int] -> Int -> Int -> Int
-checksum (x:xs) i skip =
-    if i <= 0 then 0
-    else 
-        if x == xs !! (skip-1)
-            then x + checksum xs (i-1) skip
-            else checksum xs (i-1) skip
+checksum (x:xs) i skip
+    | i <= 0                = 0
+    | x == xs !! (skip-1)   = x + checksum xs (i-1) skip
+    | otherwise             = checksum xs (i-1) skip
 
-checksum1 :: [Int] -> Int
-checksum1 xs = checksum (cycle xs) (length xs) (1)
+part1 :: [Int] -> Int
+part1 xs = checksum (cycle xs) (length xs) (1)
 
-checksum2 :: [Int] -> Int
-checksum2 xs = checksum (cycle xs) (length xs) ((length xs) `div` 2)
+part2 :: [Int] -> Int
+part2 xs = checksum (cycle xs) (length xs) ((length xs) `div` 2)
 
 main :: IO ()
 main = do 
     input <- getLine
     let list = map digitToInt input
 
-    print $ checksum1 list
-    print $ checksum2 list
+    print $ part1 list
+    print $ part2 list
