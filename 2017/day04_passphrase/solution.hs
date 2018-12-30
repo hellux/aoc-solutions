638deda0d9cf03eeb9baad20b6a53ae718f25755
Noah Hellman <noah.hellman@protonmail.com>
2018-12-30 16:49:43 +0100

    use built in filter

--- 2017/day04_passphrase/solution.hs
+++ 2017/day04_passphrase/solution.hs
@@ -1,18 +1,13 @@
 import Data.List (sort)
 
-has_doublets (x:xs)
-    | elem x xs = True
-    | otherwise = has_doublets xs
-has_doublets [] = False
+only_unique :: Eq a => [a] -> Bool
+only_unique [] = True
+only_unique (x:xs)
+    | elem x xs = False
+    | otherwise = only_unique xs
 
-part1 [] = 0
-part1 (x:xs) = (if has_doublets x then 0 else 1) + part1 xs
-
-{- check for doublets like part1 but sort characters in words first so anagrams
- - become equal -}
-part2 phrases = valids $ map (map sort) phrases
-    where valids (x:xs) = (if has_doublets x then 0 else 1) + valids xs
-          valids [] = 0
+part1 = length . (filter only_unique)
+part2 = part1 . map (map sort)
 
 main :: IO ()
 main = do
