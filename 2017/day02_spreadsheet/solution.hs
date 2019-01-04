431e946d36c4e74abe46d18a7efaf812bead6d67
Noah Hellman <noah.hellman@protonmail.com>
2019-01-05 00:03:37 +0100

    day 2: try out applicative functors

--- 2017/day02_spreadsheet/solution.hs
+++ 2017/day02_spreadsheet/solution.hs
@@ -1,16 +1,11 @@
-import Data.List (sort)
+part1 = sum . map ((-) <$> maximum <*> minimum)
 
-part1 :: [[Int]] -> Int
-part1 = sum . map (\row -> maximum row - minimum row)
-
-part2 :: [[Int]] -> Int
 part2 = sum . map q
     where q row = sum [y `div` x | y <- row, x <- row, y > x, y `mod` x == 0]
 
 main :: IO ()
 main = do
-    input <- getContents
-    let sheet = (map (map (read::String->Int) . words) . lines) input
+    sheet <- fmap (map (map read . words) . lines) getContents
 
     print $ part1 sheet
     print $ part2 sheet
