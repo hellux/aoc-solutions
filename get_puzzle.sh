3b1ef87912f29e8acafb498602e167115699d1c0
Noah Hellman <noah.hellman@protonmail.com>
2019-12-29 22:40:15 +0200

    fix urls for single digit puzzles

--- get_puzzle.sh
+++ get_puzzle.sh
@@ -14,22 +14,23 @@ JAR="cookies.jar"
 
 year="$1"
 [ -z "$year" ] && exit 1
-day="$(printf "%02d" $2)"
-[ -z "$day" ] && exit 1
+day1="$(printf "%d" $2)"
+day2="$(printf "%02d" $2)"
+[ -z "$day1" ] && exit 1
 
 if [ "$all" = "true" ]; then
-    day_dir="$(find $year -type d -name 'day'$day'_*' | head -n1)"
+    day_dir="$(find $year -type d -name 'day'$day2'_*' | head -n1)"
     if [ ! -d "$day_dir" ]; then
         printf "puzzle name: "
         read puzzle
-        day_dir=$(echo $year/day${day}_$puzzle)
+        day_dir=$(echo $year/day${day2}_$puzzle)
         mkdir $day_dir
     fi
 
     input="$day_dir/input"
 
     code=$(curl -b "$JAR" -o $input -w '%{http_code}' \
-        "https://adventofcode.com/$year/day/$day/input")
+        "https://adventofcode.com/$year/day/$day1/input")
     if [ "$code" -eq 400 ]; then
         rm "$input"
         echo "not logged in"
@@ -37,4 +38,4 @@ if [ "$all" = "true" ]; then
     fi
 fi
 
-curl -b "$JAR" "https://adventofcode.com/$year/day/$day" > "puzzle.html"
+curl -b "$JAR" "https://adventofcode.com/$year/day/$day1" > "puzzle.html"
