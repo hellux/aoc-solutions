d21f159b58c99edc5b79c71ee7059f448b9174b9
Noah Hellman <noah.hellman@protonmail.com>
2020-01-07 14:45:17 +0200

    2019 day 6 part 2

--- 2019/day06_orbits/solution.c
+++ 2019/day06_orbits/solution.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
 #define MAX_DIRECT 1700
+#define MAX_OBJECTS MAX_DIRECT*2
 #define KEYLEN 3
 #define LINELEN KEYLEN*2+2
 
@@ -30,7 +33,7 @@ void obj_add_satellite(struct object *sat, struct object *body) {
 }
 
 int objcmp(struct object *a, struct object *b) {
-    return memcmp(a->name, b->name, 3);
+    return memcmp(a->name, b->name, KEYLEN);
 }
 
 int binary_search(void *base, size_t nmemb, size_t size, void *key,
@@ -60,6 +63,42 @@ int count_orbits(struct object *obj, int depth) {
     return orbits;
 }
 
+int get_path(struct object *obj, char *name, char *path, int depth) {
+    if (strncmp(obj->name, name, KEYLEN) == 0) {
+        path[KEYLEN*depth] = '\0';
+        return 1;
+    } else {
+        for (int i = 0; i < obj->n; i++) {
+            if (get_path(obj->sats[i], name, path, depth+1)) {
+                memcpy(path+KEYLEN*depth, obj->name, KEYLEN);
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+
+int count_transits(struct object *com, char *name1, char *name2)  {
+    char path1[KEYLEN*MAX_OBJECTS];
+    get_path(com, name1, path1, 0);
+    char path2[KEYLEN*MAX_OBJECTS];
+    get_path(com, name2, path2, 0);
+
+    /* distance to each object */
+    int d1 = strlen(path1)/KEYLEN;
+    int d2 = strlen(path2)/KEYLEN;
+
+    /* distance to common parent */
+    int p = 0;
+    while (memcmp(path1+p*KEYLEN, path2+p*KEYLEN, KEYLEN) == 0 &&
+           p < MIN(d1, d2)) {
+        p++;
+    }
+
+    /* distance from object1 to parent plus parent to object2 */
+    return (d1-p) + (d2-p);
+}
+
 int main(void) {
     /* read lines */
     char direct[MAX_DIRECT][LINELEN];
@@ -69,7 +108,7 @@ int main(void) {
     }
 
     /* create objects */
-    struct object objs[MAX_DIRECT*2] = {0};
+    struct object objs[MAX_OBJECTS] = {0};
     int objc = 0;
     for (int i = 0; i < n; i++) {
         char *body = direct[i];
@@ -104,11 +143,14 @@ int main(void) {
         obj_add_satellite(&objs[i_sat], &objs[i_body]);
     }
 
-    int i_com = binary_search(objs, sizeof(struct object),
-                              objc, "COM", objcmp);
-    int orbits = count_orbits(&objs[i_com], 0);
+    int i_com = binary_search(objs, sizeof(struct object), objc, "COM", objcmp);
+    struct object *com = &objs[i_com];
+
+    int orbits = count_orbits(com, 0);
+    int transits = count_transits(com, "YOU", "SAN");
 
     printf("part1: %d\n", orbits);
+    printf("part2: %d\n", transits);
 
     return EXIT_SUCCESS;
 }
