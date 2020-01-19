8c043d5cd0a43791e6b2bef888eb49499b9b388f
Noah Hellman <noah.hellman@protonmail.com>
2020-01-19 10:30:16 +0200

    2017 day 22 part 2

--- 2017/day22_virus/solution.hs
+++ 2017/day22_virus/solution.hs
@@ -5,9 +5,9 @@ type Grid = Map (Int, Int) Node
 data Dir = East | North | West | South deriving (Show, Eq, Enum)
 data Virus = Virus (Int, Int) Dir deriving (Show)
 type World = (Virus, Grid)
-data Node = Clean | Infected deriving (Eq)
+data Node = Clean | Weakened | Infected | Flagged deriving (Eq, Enum)
 
-parseGrid rows = M.fromList $ filter ((==Infected) . snd) assocs where
+parseGrid rows = M.fromList $ filter ((== Infected) . snd) assocs where
     parseCell '.' = Clean
     parseCell '#' = Infected
     nested = map (map parseCell) rows
@@ -20,9 +20,14 @@ initialVirus = Virus (0, 0) North
 
 left  (Virus pos dir) = Virus pos (if dir == South then East  else succ dir)
 right (Virus pos dir) = Virus pos (if dir == East  then South else pred dir)
+back = right . right
 
 queryNode = M.findWithDefault Clean
-setNode = M.insert
+setNode pos Clean grid = M.delete pos grid
+setNode pos node grid = M.insert pos node grid
+
+succNode Flagged = Clean
+succNode node = succ node
 
 move (Virus (x, y) dir) = Virus newPos dir
     where newPos = case dir of
@@ -31,21 +36,31 @@ move (Virus (x, y) dir) = Virus newPos dir
                     West  -> (x-1, y)
                     South -> (x, y+1)
 
-burst (v@(Virus pos _), grid) = (newVirus, newGrid) where
-    infected = queryNode pos grid == Infected
-    rotation = if infected then right else left
-    newNode = if infected then Clean else Infected
-    newGrid = setNode pos newNode grid
+countInfections _ 0 _ = 0
+countInfections burstR n (v@(Virus pos _), grid) = infection + nextIterations
+    where newWorld@(_, newGrid) = burstR (v, grid)
+          node = queryNode pos newGrid
+          infection = if node == Infected then 1 else 0
+          nextIterations = countInfections burstR (n-1) newWorld
+
+burst rules (v@(Virus pos _), grid) = (newVirus, newGrid) where
+    node = queryNode pos grid
+    (rotation, nextNode) = rules node
+    newGrid = setNode pos nextNode grid
     newVirus = move $ rotation v
 
-countInfections 0 _ = 0
-countInfections n (v@(Virus pos _), grid) =
-    infection + countInfections (n-1) newWorld where
-    newWorld@(_, newGrid) = burst (v, grid)
-    node = queryNode pos newGrid
-    infection = if node == Infected then 1 else 0
+part1 = countInfections (burst rules) 70 where
+    rotation node = if node == Infected then right else left
+    newNode  node = if node == Infected then Clean else Infected
+    rules node = (rotation node, newNode node)
 
-part1 = countInfections 10000
+part2 = countInfections (burst rules) 100 where
+    rotation node = case node of
+                        Clean -> left
+                        Weakened -> id
+                        Infected -> right
+                        Flagged -> back
+    rules node = (rotation node, succNode node)
 
 main = do
     rows <- fmap lines getContents
@@ -53,3 +68,4 @@ main = do
     let world = (initialVirus, grid)
 
     print $ part1 world
+    print $ part2 world
