fab61d296fbe20430593a8bc6ebcfb9bce38f3b0
Noah Hellman <noah.hellman@protonmail.com>
2020-02-18 22:11:43 +0200

    2019 day 13 part 2

--- 2019/day13_intcode-breakout/solution.c
+++ 2019/day13_intcode-breakout/solution.c
@@ -3,67 +3,61 @@
 
 #include "../intcode.h"
 
-#define SCREEN_WIDTH 50
-#define SCREEN_HEIGHT 50
-
 #define EMPTY 0
 #define WALL 1
 #define BLOCK 2
 #define PADDLE 3
 #define BALL 4
 
-#define FOR_EACH_TILE(xstart, ystart, xend, yend) \
-    for (int y = ystart; y <= yend; y++) \
-        for (int x = xstart; x <= xend; x++)
-
-void show_screen(int screen[][SCREEN_WIDTH]) {
-    /* find bounding box of painted panels */
-    int xmin = SCREEN_WIDTH;  int xmax = 0;
-    int ymin = SCREEN_HEIGHT; int ymax = 0;
-    FOR_EACH_TILE(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1) {
-        if (screen[y][x] != EMPTY) {
-            if (x < xmin) xmin = x;
-            if (x > xmax) xmax = x;
-            if (y < ymin) ymin = y;
-            if (y > ymax) ymax = y;
-        }
-    }
-
-    FOR_EACH_TILE(xmin, ymin, xmax, ymax) {
-        char *tile;
-        switch (screen[y][x]) {
-        case EMPTY:     tile = " "; break;
-        case WALL:      tile = "█"; break;
-        case BLOCK:     tile = "#"; break;
-        case PADDLE:    tile = "="; break;
-        case BALL:      tile = "O"; break;
-        default: exit(1);
-        }
-        printf("%s", tile);
-        if (x == xmax)
-            printf("\n");
-    }
-}
+#define NEUTRAL 0
+#define LEFT -1
+#define RIGHT 1
 
 int part1(struct context cpu) {
-    int screen[SCREEN_HEIGHT][SCREEN_WIDTH] = {0}; 
+    int blocks = 0;
 
     while (run_until_stop(&cpu)) {
-        int x = (int) take(&cpu);
-        int y = (int) take(&cpu);
+        take(&cpu); /* read x */
+        take(&cpu); /* read y */
         int tile = (int) take(&cpu);
 
-        screen[y][x] = tile;
-    }
-
-    int blocks = 0;
-    FOR_EACH_TILE(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1) {
-        if (screen[y][x] == BLOCK)
+        if (tile == BLOCK)
             blocks++;
     }
-    show_screen(screen);
 
     return blocks;
+}
+
+int part2(struct context cpu) {
+    /* insert coin */
+    cpu.mem[0] = 2;
+
+    int paddle_x = 0;
+    int ball_x = 0;
+    int score = 0;
+    while (run_until_stop(&cpu)) {
+        if (cpu.status == STATUS_WAIT_IN) {
+            int joystick = NEUTRAL;
+
+            if (ball_x < paddle_x) {
+                joystick = LEFT;
+            } else if (paddle_x < ball_x) {
+                joystick = RIGHT;
+            }
+
+            give(&cpu, joystick);
+        }
+
+        int x = (int) take(&cpu);
+        take(&cpu); /* read y */
+        int tile = (int) take(&cpu);
+
+        if (x == -1) score = tile;
+        else if (tile == BALL)   ball_x   = x;
+        else if (tile == PADDLE) paddle_x = x;
+    }
+
+    return score;
 }
 
 int main (void) {
@@ -71,6 +65,7 @@ int main (void) {
     init_context(&ini);
 
     printf("%d\n", part1(ini));
+    printf("%d\n", part2(ini));
 
     return EXIT_SUCCESS;
 }
