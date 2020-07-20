0005965a472e4cc4dfdf4a14256c1840ecb5cff8
Noah Hellman <noah.hellman@protonmail.com>
2020-07-20 17:43:47 +0200

    2015 day 5 part 2

--- 2015/day05_strings/solution.lisp
+++ 2015/day05_strings/solution.lisp
@@ -20,9 +20,27 @@
 (defun part1 (dirs)
   (length (remove-if-not #'nicep dirs)))
 
+(defun two-pairs-p (str)
+  (and (>= (length str) 4)
+       (or (search (subseq str 0 2) (subseq str 2))
+           (two-pairs-p (subseq str 1)))))
+
+(defun repeat-inbetween-p (str)
+  (and (>= (length str) 3)
+       (or (eq (elt str 0) (elt str 2))
+           (repeat-inbetween-p (subseq str 1)))))
+
+(defun nicerp (str)
+  (and (two-pairs-p str)
+       (repeat-inbetween-p str)))
+
+(defun part2 (dirs)
+  (length (remove-if-not #'nicerp dirs)))
+
 (defun get-lines ()
   (loop for l = (read-line *standard-input* nil) until (null l) collecting l))
 
 (defun main ()
   (let ((strings (get-lines)))
-    (format t "part1: ~d~%" (part1 strings))))
+    (format t "~d~%" (part1 strings))
+    (format t "~d~%" (part2 strings))))
