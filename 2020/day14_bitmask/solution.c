0ae30fded50ec75d5ba0063af1749d7d2958ed8f
Noah Hellman <noah.hellman@protonmail.com>
2021-07-18 18:34:10 +0200

    2020 day 14 part 2

--- 2020/day14_bitmask/solution.c
+++ 2020/day14_bitmask/solution.c
@@ -7,7 +7,8 @@
 #define PROGRAM_SIZE 600
 
 typedef uint8_t op_t;
-typedef uint64_t value_t;
+typedef uint64_t val_t;
+typedef uint64_t addr_t;
 typedef uint64_t bitmask_t; // ternary number, digits 0|1|X
 
 #define OP_HALT 0
@@ -18,27 +19,54 @@ typedef struct {
     op_t op;
     union {
         struct {
-            int address;
-            value_t value;
+            uint32_t address;
+            uint32_t value;
         };
         bitmask_t bitmask;
     };
 } instr_t;
 
-typedef value_t *memory_t;
+typedef struct {
+    addr_t addr;
+    val_t val;
+} slot_t;
+
+typedef struct {
+    int count;
+    slot_t slots[MEMSIZE];
+} memory_t;
+
 typedef instr_t *program_t;
 
 typedef struct {
-    int pc;
+    uint32_t pc;
     bitmask_t bitmask;
-    memory_t memory;
+    memory_t *memory;
 } context_t;
 
+#define BIT_0 0
+#define BIT_1 1
 #define BIT_X 2
 
-value_t exp3(int exp) {
-    value_t base = 3;
-    value_t ret = 1;
+void write(memory_t *mem, addr_t a, val_t v) {
+    for (int i = 0; i < mem->count; i++) {
+        if (mem->slots[i].addr == a) {
+            mem->slots[i].val = v;
+            return;
+        }
+    }
+
+    if (mem->count >= MEMSIZE)
+        exit(4);
+
+    mem->slots[mem->count].addr = a;
+    mem->slots[mem->count].val = v;
+    mem->count++;
+}
+
+val_t exp3(int exp) {
+    val_t base = 3;
+    val_t ret = 1;
     while (1) {
         if (exp & 1) ret *= base;
         exp >>= 1;
@@ -49,21 +77,46 @@ value_t exp3(int exp) {
     return ret;
 }
 
-value_t mask_value(bitmask_t bm, value_t v) {
-    value_t ret = 0;
+val_t mask_val(bitmask_t bm, val_t v) {
+    val_t ret = 0;
     for (int i = 0; i < BITMASK_WIDTH; i++) {
-        value_t t = (bm / exp3(i)) % 3;
+        val_t t = (bm / exp3(i)) % 3;
         if (t == BIT_X) t = (v >> i) & 1;
         ret += t << i;
     }
     return ret;
 }
 
+int num_x(bitmask_t bm) {
+    int count = 0;
+    for (int i = 0; i < BITMASK_WIDTH; i++) {
+        val_t t = (bm / exp3(i)) % 3;
+        if (t == BIT_X)
+            count++;
+    }
+    return count;
+}
+
+addr_t mask_addr(bitmask_t bm, addr_t a, val_t f) {
+    val_t ret = 0;
+    int i_floating = 0;
+    for (int i = 0; i < BITMASK_WIDTH; i++) {
+        val_t x = 0;
+        switch ((bm / exp3(i)) % 3) {
+            case BIT_0: x = (a >> i) & 1; break;
+            case BIT_1: x = 1; break;
+            case BIT_X: x = (f >> i_floating++) & 1; break;
+        }
+        ret += x << i;
+    }
+    return ret;
+}
+
 bitmask_t parse_bitmask(char *str) {
-    value_t bm = 0;
+    val_t bm = 0;
     for (int i = 0; i < BITMASK_WIDTH; i++) {
         int pos = BITMASK_WIDTH-i-1;
-        value_t trit;
+        val_t trit;
         switch (str[i]) {
             case '0': trit = 0; break;
             case '1': trit = 1; break;
@@ -83,13 +136,12 @@ program_t parse_program() {
     int i = 0;
     while (fgets(line, 100, stdin) != NULL) {
         instr_t instr;
-        int addr;
-        value_t val;
-        char bm[36];
+        uint32_t addr, val;
+        char bm[BITMASK_WIDTH];
         if (sscanf(line, "mask = %s\n", bm)) {
             instr.op = OP_SET_MASK;
             instr.bitmask = parse_bitmask(bm);
-        } else if (sscanf(line, "mem[%d] = %lu\n", &addr, &val)) {
+        } else if (sscanf(line, "mem[%u] = %u\n", &addr, &val)) {
             instr.op = OP_WRITE;
             instr.value = val;
             instr.address = addr;
@@ -102,43 +154,48 @@ program_t parse_program() {
     return program;
 }
 
-void context_execute(context_t *ctx, const program_t prgm) {
+val_t context_execute(const program_t prgm, int part) {
+    context_t ctx;
+    ctx.pc = 0;
+    ctx.bitmask = 0;
+    ctx.memory = malloc(sizeof(memory_t));
+
     while (1) {
-        instr_t instr = prgm[ctx->pc++];
+        instr_t instr = prgm[ctx.pc++];
         switch (instr.op) {
             case OP_HALT:
-                return;
+                goto halt;
             case OP_SET_MASK:
-                ctx->bitmask = instr.bitmask;
+                ctx.bitmask = instr.bitmask;
                 break;
             case OP_WRITE:
-                value_t masked = mask_value(ctx->bitmask, instr.value);
-                ctx->memory[instr.address] = masked;
-                break;
+                if (part == 1) {
+                    val_t masked = mask_val(ctx.bitmask, instr.value);
+                    write(ctx.memory, instr.address, masked);
+                } else {
+                    val_t floating = (val_t)1 << num_x(ctx.bitmask);
+                    for (val_t f = 0; f < floating; f++) {
+                        addr_t addr = mask_addr(ctx.bitmask, instr.address, f);
+                        write(ctx.memory, addr, instr.value);
+                    }
+                }
         }
     }
-}
 
-value_t memory_sum(memory_t mem) {
-    value_t ret = 0;
-    for (int i = 0; i < MEMSIZE; i++) {
-        ret += mem[i];
+halt:
+    val_t sum = 0;
+    for (int i = 0; i < ctx.memory->count; i++) {
+        sum += ctx.memory->slots[i].val;
     }
-    return ret;
+
+    free(ctx.memory);
+
+    return sum;
 }
 
 int main() {
     program_t prgm = parse_program();
-
-    context_t ctx;
-    ctx.pc = 0;
-    ctx.bitmask = 0;
-    ctx.memory = calloc(MEMSIZE, sizeof(value_t));
-
-    context_execute(&ctx, prgm);
-    value_t sum = memory_sum(ctx.memory);
-    printf("%lu\n", sum);
-
+    printf("%lu\n", context_execute(prgm, 1));
+    printf("%lu\n", context_execute(prgm, 2));
     free(prgm);
-    free(ctx.memory);
 }
