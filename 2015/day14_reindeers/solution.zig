a51b46cfa12bcfe72f430d07ca2ef0f56957f99c
Noah Hellman <noah.hellman@protonmail.com>
2021-07-26 23:23:30 +0200

    2015 day 14 part 2

--- 2015/day14_reindeers/solution.zig
+++ 2015/day14_reindeers/solution.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const TIME: u32 = 2503;
+const MAX_REINDEERS = 20;
+
 const Reindeer = struct {
     speed: u32,
     time_move: u32,
@@ -36,31 +39,57 @@ pub fn parse_input(reindeers: [*]Reindeer) ![]Reindeer {
     return reindeers[0..i];
 }
 
-pub fn distance(r: Reindeer, time: u32) u32 {
-    const period = r.time_move + r.time_rest;
-    const full_cycles = time / period;
-    const dist_full_cycles = full_cycles * r.speed * r.time_move;
-    const time_last = time % period;
-    const dist_last = r.speed * std.math.min(r.time_move, time_last);
-
-    return dist_full_cycles + dist_last;
-}
-
 pub fn part1(reindeers: []Reindeer) u32 {
     var max_distance: u32 = 0;
 
     for (reindeers) |r| {
-        const d = distance(r, 2503);
-        max_distance = std.math.max(d, max_distance);
+        const period = r.time_move + r.time_rest;
+        const full_cycles = TIME / period;
+        const dist_full_cycles = full_cycles * r.speed * r.time_move;
+        const time_last = TIME % period;
+        const dist_last = r.speed * std.math.min(r.time_move, time_last);
+        const distance = dist_full_cycles + dist_last;
+
+        max_distance = std.math.max(distance, max_distance);
     }
 
     return max_distance;
 }
 
+pub fn part2(reindeers: []Reindeer) !u32 {
+    var positions: [MAX_REINDEERS]u32 = [_]u32{0} ** MAX_REINDEERS;
+    var scores: [MAX_REINDEERS]u32 = [_]u32{0} ** MAX_REINDEERS;
+
+    const c = reindeers.len;
+
+    var t: u32 = 0;
+    while (t < TIME) : (t += 1) {
+        var j: usize = 0;
+        while (j < c) : (j += 1) {
+            const r = reindeers[j];
+            const period = r.time_move + r.time_rest;
+            const moving = (t % period) < r.time_move;
+            if (moving) {
+                positions[j] += r.speed;
+            }
+        }
+
+        const max_distance = std.mem.max(u32, positions[0..c]);
+        var k: usize = 0;
+        while (k < c) : (k += 1) {
+            if (positions[k] == max_distance) {
+                scores[k] += 1;
+            }
+        }
+    }
+
+    return std.mem.max(u32, scores[0..c]);
+}
+
 pub fn main() !void {
-    var buf_reindeers: [20]Reindeer = undefined;
+    var buf_reindeers: [MAX_REINDEERS]Reindeer = undefined;
     const reindeers = try parse_input(&buf_reindeers);
 
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("{d}\n", .{part1(reindeers)});
+    try stdout.print("{d}\n{d}\n", .{ part1(reindeers), part2(reindeers) });
 }
