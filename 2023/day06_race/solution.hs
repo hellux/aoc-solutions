84bb51c742b52e90cbf7d9a3940378afdb6ac277
Noah Hellman <noah@hllmn.net>
2023-12-06 23:09:58 +0100

    2023 day 6 part 1

---
+++ 2023/day06_race/solution.hs
@@ -0,0 +1,25 @@
+import Data.List (groupBy, transpose)
+import Data.Char (isDigit)
+
+parseInput = map (\[x,y] -> (x,y))
+        . transpose
+        . map (map read . filter (isDigit . head)
+        . groupBy (\x y -> isDigit x == isDigit y)
+        . dropWhile (' ' ==)
+        . drop 10)
+    . filter (not . null)
+    . lines
+
+-- solve v(v-t) > d for v
+bounds (t, d) = (floor $ (tf - s) / 2 + 1, ceiling $ (tf + s) / 2 - 1) where
+    tf = fromIntegral t :: Float
+    df = fromIntegral d :: Float
+    s = sqrt (tf*tf - 4*df)
+
+race = (\(l,u) -> u-l+1) . bounds
+
+part1 = product . map race
+
+main = do
+    input <- fmap parseInput getContents
+    print $ part1 input
