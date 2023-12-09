d7be7d878c0047bbcb35948bc9d4d02d512feeed
Noah Hellman <noah@hllmn.net>
2023-12-09 13:44:32 +0100

    2023 day 9 part 2

--- 2023/day09_extrapolate/solution.hs
+++ 2023/day09_extrapolate/solution.hs
@@ -6,10 +6,17 @@ next x
     | otherwise = l + next (diffs x)
     where l = last x
 
+prev x
+    | all (== l) x = l
+    | otherwise = l - prev (diffs x)
+    where l = head x
+
 diffs x = zipWith (-) (tail x) x
 
 part1 = sum . map next
+part2 = sum . map prev
 
 main = do
     input <- fmap parseInput getContents
     print $ part1 input
+    print $ part2 input
