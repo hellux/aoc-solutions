8ab262d851f0947d90f8020b7a62264f4194a77f
Noah Hellman <noah@hllmn.net>
2023-12-10 12:12:58 +0100

    zig: make compilable for 0.11

--- 2015/day14_reindeers/solution.zig
+++ 2015/day14_reindeers/solution.zig
@@ -15,8 +15,8 @@ pub fn parse_input(reindeers: [*]Reindeer) ![]Reindeer {
     var buf_line: [100]u8 = undefined;
     var i: usize = 0;
 
-    while (try stdin.readUntilDelimiterOrEof(buf_line[0..], '\n')) |line| {
-        var iter = std.mem.split(buf_line[0..], " ");
+    while (try stdin.readUntilDelimiterOrEof(buf_line[0..], '\n')) |_| {
+        var iter = std.mem.split(u8, buf_line[0..], " ");
         _ = iter.next(); // <name>
         _ = iter.next(); // can
         _ = iter.next(); // run
@@ -47,10 +47,10 @@ pub fn part1(reindeers: []Reindeer) u32 {
         const full_cycles = TIME / period;
         const dist_full_cycles = full_cycles * r.speed * r.time_move;
         const time_last = TIME % period;
-        const dist_last = r.speed * std.math.min(r.time_move, time_last);
+        const dist_last = r.speed * @min(r.time_move, time_last);
         const distance = dist_full_cycles + dist_last;
 
-        max_distance = std.math.max(distance, max_distance);
+        max_distance = @max(distance, max_distance);
     }
 
     return max_distance;
@@ -91,5 +91,5 @@ pub fn main() !void {
     const reindeers = try parse_input(&buf_reindeers);
 
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("{d}\n{d}\n", .{ part1(reindeers), part2(reindeers) });
+    try stdout.print("{!}\n{!}\n", .{ part1(reindeers), part2(reindeers) });
 }
