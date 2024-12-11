5fa3cab9c66a9f24971ef546f60759154ee73af3
Noah Hellman <noah@hllmn.net>
2024-12-11 20:22:18 +0100

    2024 day 4 part 2

--- 2024/day04_wordsoup/solution.c
+++ 2024/day04_wordsoup/solution.c
@@ -58,12 +58,55 @@ size_t part1(char (*grid)[MAX_SIZE][MAX_SIZE], const size_t w, const size_t h) {
     return n;
 }
 
+bool cross_mas(char (*grid)[MAX_SIZE][MAX_SIZE], const size_t x, const size_t y) {
+    static const char *MAS[] = {"MAS", "SAM"};
+    static size_t L = 3;
+
+    for (size_t d = 0; d < 2; d++) {
+        bool line_match = false;
+        for (size_t i = 0; i < sizeof(MAS) / sizeof(MAS[0]); i++) {
+            bool dir_match = true;
+            for (size_t j = 0; j < L; j++) {
+                const size_t xx = d == 0 ? y + j : y + L - j - 1;
+                const size_t yy = x + j;
+                if ((*grid)[yy][xx] != MAS[i][j]) {
+                    dir_match = false;
+                    break;
+                }
+            }
+            if (dir_match) {
+                line_match = true;
+                break;
+            }
+        }
+        if (!line_match)
+            return false;
+    }
+
+    return true;
+}
+
+size_t part2(char (*grid)[MAX_SIZE][MAX_SIZE], const size_t w, const size_t h) {
+    size_t n = 0;
+
+    for (size_t y = 0; y < h; y++) {
+        for (size_t x = 0; x < w; x++) {
+            n += cross_mas(grid, x, y);
+        }
+    }
+
+    return n;
+}
+
 int main() {
     char grid[MAX_SIZE][MAX_SIZE];
     size_t h = 0;
     while (fgets(grid[h], MAX_SIZE, stdin))
         h++;
     const size_t w = strlen(grid[0]) - 1;
+    for (size_t x = 0; x < w; x++)
+        grid[h][x] = '\0';
 
     printf("%lu\n", part1(&grid, w, h));
+    printf("%lu\n", part2(&grid, w, h));
 }
