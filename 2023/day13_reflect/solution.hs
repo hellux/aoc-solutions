3133f9725958cdfabc7f7aa75442e0f79fdb2e40
Noah Hellman <noah@hllmn.net>
2024-12-12 22:39:54 +0100

    2023 day 13: avoid 3rd party List.Split lib

--- 2023/day13_reflect/solution.hs
+++ 2023/day13_reflect/solution.hs
@@ -1,6 +1,10 @@
-import Data.List.Split (splitOn)
-import Data.List (transpose, find)
+import Data.List (transpose, find, elemIndex)
 import Data.Maybe (fromJust)
+
+splitOn c x = case elemIndex c x of
+    Nothing -> [x]
+    Just i -> let (y, ys) = splitAt i x
+              in y : splitOn c (drop 1 ys)
 
 horiRefl rows = find r [1..length rows-1] where
     r i = let (a, b) = splitAt i rows
@@ -11,5 +15,5 @@ p1 rows = maybe (fromJust $ vertRefl rows) (*100) (horiRefl rows)
 part1 = sum . map p1
 
 main = do
-    input <- fmap (splitOn [""] . lines) getContents
+    input <- fmap (splitOn "" . lines) getContents
     print $ part1 input
