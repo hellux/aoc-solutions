17a80ca930692b30f68269bab0b400e9570a42f7
Noah Hellman <noah@hllmn.net>
2024-12-12 22:30:33 +0100

    2024 day 5 part 2

--- 2024/day05_pageorder/solution.hs
+++ 2024/day05_pageorder/solution.hs
@@ -12,10 +12,14 @@ parse ls = ( map parseRule (filter (elem '|') ls)
     parseRule = (\[a,b] -> (a, b)) . map read . splitOn '|'
     parsePage = map read . splitOn ','
 
-part1 rules = sum . map (\xs -> xs !! (div (length xs) 2)) . filter isSorted where
-    cmp a b = if elem (a, b) rules then LT else GT
-    isSorted ls = sortBy cmp ls == ls
+cmp rules a b = if elem (a, b) rules then LT else GT
+middle xs = xs !! (div (length xs) 2)
+isSorted rules ls = sortBy (cmp rules) ls == ls
+
+part1 rules = sum . map middle . filter (isSorted rules)
+part2 rules = sum . map (middle . sortBy (cmp rules)) . filter (not . (isSorted rules))
 
 main = do
     (rules, pages) <- fmap (parse . lines) getContents
     print $ part1 rules pages
+    print $ part2 rules pages
