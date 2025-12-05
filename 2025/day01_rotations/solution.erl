6adfda03107b615e85dd24c200de9fa0b58887c7
Noah Hellman <noah@hllmn.net>
2025-12-06 00:23:18 +0100

    2025 day 1 part 2

--- 2025/day01_rotations/solution.erl
+++ 2025/day01_rotations/solution.erl
@@ -17,6 +17,16 @@ p1z(Dial, [Rot | Ations]) ->
     p1z(DialNext, Ations).
 p1(Rotations) -> p1z(50, Rotations).
 
+p2z(_, []) -> 0;
+p2z(Dial, [Rot | Ations]) ->
+    DialNext = Dial + Rot,
+    Step = if Dial < DialNext -> 1; true -> -1 end,
+    AllDials = lists:seq(Dial+Step, DialNext, Step),
+    length(lists:filter(fun(N) -> N rem 100 =:= 0 end, AllDials)) +
+    p2z(DialNext rem 100, Ations).
+p2(Rotations) -> p2z(50, Rotations).
+
 main(_) ->
     Rotations = get_input(),
-    io:format("~p~n", [p1(Rotations)]).
+    io:format("~p~n", [p1(Rotations)]),
+    io:format("~p~n", [p2(Rotations)]).
