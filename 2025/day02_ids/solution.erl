91525e8f4e67712d2177d304f0ad9726a3f3c1f2
Noah Hellman <noah@hllmn.net>
2025-12-07 00:32:49 +0100

    2025 day 2 part 2

--- 2025/day02_ids/solution.erl
+++ 2025/day02_ids/solution.erl
@@ -17,10 +17,35 @@ all_ids(Ranges) -> lists:flatmap(fun({A, B}) -> lists:seq(A, B) end, Ranges).
 
 p1(Ranges) ->
     InvalidIds = lists:filter(
+        fun(Id) -> repeats_n_times(2, integer_to_list(Id)) end,
+        all_ids(Ranges)
+    ),
+    lists:sum(InvalidIds).
+
+% divide into lists of len N, e.g.
+%   [[a, b, c], [d, e, f]] = split_all(3, [a, b, c, d, e, f])
+split_all(_, []) -> [];
+split_all(N, List) ->
+    {X, Xs} = lists:split(N, List),
+    [X | split_all(N, Xs)].
+
+repeats_n_times(N, List) ->
+    Len = length(List),
+    if
+        Len rem N =:= 0 ->
+            Xs = split_all(length(List) div N, List),
+            lists:all(fun(X) -> X =:= lists:nth(1, Xs) end, Xs);
+        true -> false
+    end.
+
+p2(Ranges) ->
+    InvalidIds = lists:filter(
         fun(Id) ->
             Str = integer_to_list(Id),
-            {A, B} = lists:split(length(Str) div 2, Str),
-            A =:= B
+            lists:any(
+                fun(N) -> repeats_n_times(N, Str) end,
+                lists:seq(2, length(Str))
+            )
         end,
         all_ids(Ranges)
     ),
@@ -28,4 +53,4 @@ p1(Ranges) ->
 
 main(_) ->
     Ranges = get_input(),
-    io:format("~p~n", [p1(Ranges)]).
+    io:format("~p~n~p~n", [p1(Ranges), p2(Ranges)]).
